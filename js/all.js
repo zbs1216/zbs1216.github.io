f76df670672c3810f8bd5927710e5bc24ac2310e
zbs1216 <971763064@qq.com>
2018-03-25 13:03:55 +0800

    ..

--- js/all.js
+++ js/all.js
@@ -12,9 +12,10 @@ function getText(url, callback) {
     request.open("GET", url);
     
 	request.onreadystatechange = function() {
-        if (request.readystate === 4 && request.status === 200) {
+        
             callback(request.responseText);
-		}
+			console.log(request)
+		
     }; 
     
     
